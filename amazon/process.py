0ec792e2e765be7db02b739b3f7d6b6075b3ecb3
Alex Dehnert <adehnert@mit.edu>
2024-03-28 03:04:21 -0400

    amazon: First pass at incorporating credit card payments

--- amazon/process.py
+++ amazon/process.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
 
+import collections
 import csv
 from dataclasses import dataclass
-from datetime import datetime
+from datetime import datetime, timedelta
 from decimal import Decimal
 import logging
 import pprint
+import re
 import sys
 
 from typing import Dict, List, Any
 
-import gnucash
+import gnucash # type: ignore
 
 LOGGER = logging.getLogger(__name__)
 
@@ -25,7 +27,7 @@ class LineItem:
     desc: str
     is_gift: bool
     data: dict      # Underlying CSV/JSON/etc. data for this line item
-    
+
     @classmethod
     def from_amazon_csv(cls, item):
         # Money related columns for Amazon:
@@ -62,15 +64,17 @@ class Order:
 
     def update_payment(self):
         self.total = sum(item.cost for item in self.items)
+        self.date = min(item.date for item in self.items)
         item = self.items[0]
         payment = item.data['Payment Instrument Type']
         payments = payment.split(' and ')
-        print(self.total)
         self.payments = [(instrument, Decimal(self.total if i == 0 else 0)) for i, instrument in enumerate(payments)]
+        if len(self.payments) > 2:
+            LOGGER.warning("Many payments: %s %s", self.payments, self)
 
 def load_csv(fp) -> Dict[str,Order]:
     reader = csv.DictReader(fp)
-    orders = {}
+    orders: Dict[str,Order] = {}
     for line in reader:
         if line['Order Status'] == 'Cancelled':
             LOGGER.debug("Skipping cancelled order: %s", line)
@@ -84,11 +88,112 @@ def load_csv(fp) -> Dict[str,Order]:
             order = Order([], [], Decimal(0))
             orders[order_id] = order
         order.items.append(LineItem.from_amazon_csv(line))
+    payment_count = collections.Counter()
+    payment_instruments = collections.Counter()
     for order_id, order in orders.items():
         order.update_payment()
+        payment_count[len(order.payments)] += 1
+        for payment, amount in order.payments:
+            payment_instruments[payment] += 1
+    LOGGER.info("payment counts: %s", payment_count)
+    LOGGER.info("payment instruments: %s", payment_instruments)
     return orders
+
+# Gift card transaction history:
+# https://www.amazon.com/gc/balance
+
+# GnuCash portion
+
+ACCT_NUM_REGEX = re.compile(r'(?P<num>\d{4})\D')
+
+#@dataclass
+#class AccountInfo:
+#    acct: gnucash.Account
+#    trans: List[gnucash.Transaction]
+
+def get_creditcard_accounts(root: gnucash.Account) -> Dict[str,gnucash.Account]:
+    accts = root.lookup_by_full_name('Credit Cards').get_descendants()
+    acct_map = {}
+    for acct in accts:
+        match = ACCT_NUM_REGEX.search(acct.GetName())
+        if match:
+            num = match.group('num')
+            if num == '1008': # Two AmEx accounts, fortunately little-used
+                LOGGER.debug("Ignoring known-dup account number: %s", acct.GetName())
+                continue
+            assert num not in acct_map
+            acct_map[match.group('num')] = acct #AccountInfo(acct, [])
+        else:
+            LOGGER.debug("Unknown last-4 for account: %s", acct.GetName())
+    acct_map['Gift Certificate/Card'] = root.lookup_by_full_name('Assets.Current Assets.Gift cards.Amazon balance')
+    return acct_map
+
+def gnc_to_decimal(amt):
+    assert amt.denom() == 100
+    return Decimal(amt.num()) / amt.denom()
+
+def split_tuple(split):
+    """Helper function to convert a split into a printable tuple"""
+    splits = [(split1.GetAccount().get_full_name(), split1.GetAmount().to_double()) for split1 in split.parent.GetSplitList()]
+    return split.parent.GetDate(), len(split.parent.GetSplitList()), splits
+
+def match_account(instrument, accts):
+    """Match account based on payment instrument from import to candidate accounts"""
+    acct = accts.get(instrument)
+    if acct: return acct
+    _network, _dash, num = instrument.partition(' - ')
+    return accts.get(num)
+
+def near_date(order_date, cc_date):
+    """See if order date and credit card transaction date are plausibly close"""
+    return ((order_date - timedelta(days=2) <= cc_date) and
+            (order_date + timedelta(days=14) > cc_date))
+
+def match_splits(acct, cc_accts, orders):
+    splits = acct.GetSplitList()
+
+    # Build a map of CC account to CC splits that need to be matched
+    acct_trans = {cc_acct.get_full_name(): [] for cc_acct in cc_accts.values()}
+    for split in splits:
+        LOGGER.info(split_tuple(split))
+        for other_split in split.parent.GetSplitList():
+            acct_trans.get(other_split.GetAccount().get_full_name(), []).append(other_split)
+
+    for order_id, order in orders.items():
+        if len(order.payments) == 1:
+            #LOGGER.info("considering order %s", order)
+            instrument, amount = order.payments[0]
+            other_acct = match_account(instrument, cc_accts)
+            if not other_acct:
+                LOGGER.warning('unknown account %s', instrument)
+                continue # next order
+            other_acct_name = other_acct.get_full_name()
+            match_splits = acct_trans[other_acct_name]
+            for match_split in match_splits:
+                match_amount = -1 * gnc_to_decimal(match_split.GetAmount())
+                if (amount == match_amount and
+                    near_date(order.date, match_split.parent.GetDate())):
+                    # Splits match!
+                    LOGGER.info("matched transaction: %s=%s, %s to %s",
+                                amount, match_amount, order, split_tuple(match_split))
+        else:
+            LOGGER.info("ignoring order with multiple payments %s: %s", order.payments, order)
+            # Same basic algorithm, except any smaller payment is reasonable
+            # (but the gift card balancing split we invent)
+
+def gnucash_import(filename, orders):
+    book_uri = 'file://' + filename
+    mode = gnucash.SessionOpenMode.SESSION_NORMAL_OPEN
+    mode = gnucash.SessionOpenMode.SESSION_READ_ONLY
+    with gnucash.Session(book_uri=book_uri, mode=mode) as session:
+        root = session.get_book().get_root_account()
+        accts = get_creditcard_accounts(root)
+        LOGGER.info('CC accounts %s', accts)
+        imbalance = root.lookup_by_full_name('Imbalance-USD')
+        match_splits(imbalance, accts, orders)
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
-    data = load_csv(sys.stdin)
-    print(pprint.pformat(data, width=120))
+    orders = load_csv(sys.stdin)
+    print(pprint.pformat(orders, width=120))
+    gnucash_import(sys.argv[1], orders)
